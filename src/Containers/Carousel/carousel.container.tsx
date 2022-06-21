ed3cbb15746ded6f95271bc4bfe9da63d024e970
aynorica <amirdeilamizadeh1996@gmail.com>
2022-06-21 09:25:21 -0700

    update

--- src/Containers/Carousel/carousel.container.tsx
+++ src/Containers/Carousel/carousel.container.tsx
@@ -103,67 +103,67 @@ const sizeCalculator = (width:number) => {
 
 export const nftItems:NftShowcaseComponentInput[] = [
     {
-        img: 'https://imagedelivery.net/9htEsbM85VXhE8lOYQH9iw/4593a4ad-54b8-42f3-4d79-484eba340d00/public',
+        img: 'https://imagedelivery.net/9htEsbM85VXhE8lOYQH9iw/88f9749b-5f3d-422f-4b57-36e82acf5500/public',
         name: 'TIREDOPIA #1',
         alt: 'TIREDOPIA #1'
     },
     {
-        img: 'https://imagedelivery.net/9htEsbM85VXhE8lOYQH9iw/61341508-bf01-4f3e-6024-3136b5197e00/public',
+        img: 'https://imagedelivery.net/9htEsbM85VXhE8lOYQH9iw/a0a9c34c-b4ce-4035-8505-df1420f85100/public',
         name: 'TIREDOPIA #2',
         alt: 'TIREDOPIA #2'
     },
     {
-        img: 'https://imagedelivery.net/9htEsbM85VXhE8lOYQH9iw/c870f8a1-54ce-4807-5aa3-1beaf89d7100/public',
+        img: 'https://imagedelivery.net/9htEsbM85VXhE8lOYQH9iw/34c32b58-bca6-4180-b3c5-b37466f6f700/public',
         name: 'TIREDOPIA #3',
         alt: 'TIREDOPIA #3'
     },
     {
-        img: 'https://imagedelivery.net/9htEsbM85VXhE8lOYQH9iw/03f964ea-842d-4480-1fca-0ca9fdfb2500/public',
+        img: 'https://imagedelivery.net/9htEsbM85VXhE8lOYQH9iw/50751d73-8b02-4973-13ef-83acbe986a00/public',
         name: 'TIREDOPIA #4',
         alt: 'TIREDOPIA #4'
     },
     {
-        img: 'https://imagedelivery.net/9htEsbM85VXhE8lOYQH9iw/ffd6a833-fca2-4d15-b118-7bf42f409c00/public',
+        img: 'https://imagedelivery.net/9htEsbM85VXhE8lOYQH9iw/f546787d-4629-43f5-1470-51a6d898b700/public',
         name: 'TIREDOPIA #5',
         alt: 'TIREDOPIA #5'
     },
     {
-        img: 'https://imagedelivery.net/9htEsbM85VXhE8lOYQH9iw/e8297e6e-7924-4223-8869-c61280750f00/public',
+        img: 'https://imagedelivery.net/9htEsbM85VXhE8lOYQH9iw/cf0f2696-32c7-4cd4-a334-54dccc471800/public',
         name: 'TIREDOPIA #6',
         alt: 'TIREDOPIA #6'
     },
     {
-        img: 'https://imagedelivery.net/9htEsbM85VXhE8lOYQH9iw/572df88f-332c-48ff-c3ff-a5482fa94d00/public',
+        img: 'https://imagedelivery.net/9htEsbM85VXhE8lOYQH9iw/494df67a-4388-4df4-6566-178d079a6200/public',
         name: 'TIREDOPIA #7',
         alt: 'TIREDOPIA #7'
     },
     {
-        img: 'https://imagedelivery.net/9htEsbM85VXhE8lOYQH9iw/aeded697-ba89-4824-025c-09b573349b00/public',
+        img: 'https://imagedelivery.net/9htEsbM85VXhE8lOYQH9iw/127d2bc3-596c-4f3b-5571-1363f6bfff00/public',
         name: 'TIREDOPIA #8',
         alt: 'TIREDOPIA #8'
     },
     {
-        img: 'https://imagedelivery.net/9htEsbM85VXhE8lOYQH9iw/c5262cb7-7d49-4aed-5058-9ee7d1bbe100/public',
+        img: 'https://imagedelivery.net/9htEsbM85VXhE8lOYQH9iw/5e295414-4c3e-4bbc-9174-f8f54d886b00/public',
         name: 'TIREDOPIA #9',
         alt: 'TIREDOPIA #9'
     },
     {
-        img: 'https://imagedelivery.net/9htEsbM85VXhE8lOYQH9iw/01727d9a-c08f-4784-d79a-baf603ac6400/public',
+        img: 'https://imagedelivery.net/9htEsbM85VXhE8lOYQH9iw/c76422ad-553c-47e5-e802-c61b44ff4b00/public',
         name: 'TIREDOPIA #10',
         alt: 'TIREDOPIA #10'
     },
     {
-        img: 'https://imagedelivery.net/9htEsbM85VXhE8lOYQH9iw/60cb99d4-3992-4b5e-0d72-01bd2a84e500/public',
+        img: 'https://imagedelivery.net/9htEsbM85VXhE8lOYQH9iw/6b0e692b-eb78-42a0-e33c-c810a334b600/public',
         name: 'TIREDOPIA #11',
         alt: 'TIREDOPIA #11'
     },
     {
-        img: 'https://imagedelivery.net/9htEsbM85VXhE8lOYQH9iw/8ed48762-f7df-4260-2dac-f7175375c700/public',
+        img: 'https://imagedelivery.net/9htEsbM85VXhE8lOYQH9iw/d011f11e-5b81-48a5-46d2-f3b825937600/public',
         name: 'TIREDOPIA #12',
         alt: 'TIREDOPIA #12'
     },
     {
-        img: 'https://imagedelivery.net/9htEsbM85VXhE8lOYQH9iw/39591441-6770-49aa-3355-bf0295f03500/public',
+        img: 'https://imagedelivery.net/9htEsbM85VXhE8lOYQH9iw/91d69e26-d8c1-462f-c548-771d0fce5500/public',
         name: 'TIREDOPIA #13',
         alt: 'TIREDOPIA #13'
     },
@@ -173,37 +173,37 @@ export const nftItems:NftShowcaseComponentInput[] = [
         alt: 'TIREDOPIA #13'
     },
     {
-        img: 'https://imagedelivery.net/9htEsbM85VXhE8lOYQH9iw/b15454b7-2905-4680-7814-9006807e3700/public',
+        img: 'https://imagedelivery.net/9htEsbM85VXhE8lOYQH9iw/81dd47f0-5b50-464d-c401-132b39ab9e00/public',
         name: 'TIREDOPIA #14',
         alt: 'TIREDOPIA #14'
     },
     {
-        img: 'https://imagedelivery.net/9htEsbM85VXhE8lOYQH9iw/ffd6a833-fca2-4d15-b118-7bf42f409c00/public',
+        img: 'https://imagedelivery.net/9htEsbM85VXhE8lOYQH9iw/887727e0-e3f5-4725-b245-c7e1c8e8f300/public',
         name: 'TIREDOPIA #15',
         alt: 'TIREDOPIA #15'
     },
     {
-        img: 'https://imagedelivery.net/9htEsbM85VXhE8lOYQH9iw/c5262cb7-7d49-4aed-5058-9ee7d1bbe100/public',
+        img: 'https://imagedelivery.net/9htEsbM85VXhE8lOYQH9iw/2cfdae33-95bd-4192-b916-102afbfd7900/public',
         name: 'TIREDOPIA #16',
         alt: 'TIREDOPIA #16'
     },
     {
-        img: 'https://imagedelivery.net/9htEsbM85VXhE8lOYQH9iw/61341508-bf01-4f3e-6024-3136b5197e00/public',
+        img: 'https://imagedelivery.net/9htEsbM85VXhE8lOYQH9iw/419c8907-a599-42b3-bfa7-36f4e5224600/public',
         name: 'TIREDOPIA #17',
         alt: 'TIREDOPIA #17'
     },
     {
-        img: 'https://imagedelivery.net/9htEsbM85VXhE8lOYQH9iw/4593a4ad-54b8-42f3-4d79-484eba340d00/public',
+        img: 'https://imagedelivery.net/9htEsbM85VXhE8lOYQH9iw/baf006fd-e9c2-45ac-59a2-94733d443a00/public',
         name: 'TIREDOPIA #18',
         alt: 'TIREDOPIA #18'
     },
     {
-        img: 'https://imagedelivery.net/9htEsbM85VXhE8lOYQH9iw/39591441-6770-49aa-3355-bf0295f03500/public',
+        img: 'https://imagedelivery.net/9htEsbM85VXhE8lOYQH9iw/81ba76fd-13ed-404e-d005-cb9cdc644c00/public',
         name: 'TIREDOPIA #19',
         alt: 'TIREDOPIA #19'
     },
     {
-        img: 'https://imagedelivery.net/9htEsbM85VXhE8lOYQH9iw/8ed48762-f7df-4260-2dac-f7175375c700/public',
+        img: 'https://imagedelivery.net/9htEsbM85VXhE8lOYQH9iw/e706b9e9-5730-4584-7c2a-a124f891d100/public',
         name: 'TIREDOPIA #20',
         alt: 'TIREDOPIA #20'
     },
