f3f4ca06fdb1d33d5ae92dda916120eaba1b3b97
aynorica <amirdeilamizadeh1996@gmail.com>
2022-07-09 11:32:50 -0700

    update

--- src/Containers/AboutDescription/aboutDesc.container.tsx
+++ src/Containers/AboutDescription/aboutDesc.container.tsx
@@ -5,6 +5,7 @@ import {IntroSubHeaderComponent} from "../../Components/IntroSubHeader/introSubH
 import {SpecialBtnComponent} from "../../Components/Buttons/Special Button/specialBtn.component";
 import {TitleComponent, TitleSmallComponent} from "../../Components/Title/title.component";
 import {AboutContainerInputs} from "../About/about.container";
+import {TwitterLogoSvg} from "../../Assets/Logo/twitter-logo";
 
 export const AboutDescContainer:React.FC<AboutContainerInputs> = (data:AboutContainerInputs) => {
     const { main } = data;
@@ -20,7 +21,7 @@ export const AboutDescContainer:React.FC<AboutContainerInputs> = (data:AboutCont
                 <IntroSubHeaderComponent text={"Growing up in a restricted community, feeling limited under circumstances was always a problem for us. Leading to us wanting to create a world where people would not be limited to conditions. A world where people can have their own land and create freely without any limitations. "}/>
             </div>
             <div className={`about-desc-container-btn about-desc-container-center ${main ? null : "about-btn-hidden"}`}>
-                <SpecialBtnComponent link={"https://opensea.io/collection/tiredopia"} text={"Buy one"} fill={false} scale={1.1} />
+                <SpecialBtnComponent link={"https://opensea.io/collection/tiredopia"} text={"Follow us"} fill={false} scale={1.1} icon={<TwitterLogoSvg />} />
             </div>
         </div>
     )
